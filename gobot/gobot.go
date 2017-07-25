c768f861a02ab0d1c73ba9356360948d74b22143
banjocat <jackmuratore@gmail.com>
2017-07-25 17:19:37 -0400

    Start of websocket

--- gobot/gobot.go
+++ gobot/gobot.go
@@ -4,7 +4,16 @@ import (
     "log"
     "os"
     "net/http"
+    "encoding/json"
+    "golang.org/x/net/websocket"
 )
+
+type SlackAuthResponse struct{
+    Ok bool
+    Url string
+    Team map[string]*string
+}
+
 
 const slackRtmAuthorize = "https://slack.com/api/rtm.connect"
 
@@ -15,12 +24,32 @@ func main() {
     parse messages directed at bot
     send repsonses based on regex
     */
+    client := &http.Client{}
     request, err := http.NewRequest("GET", slackRtmAuthorize, nil)
     if (err != nil) {
-	log.Fatal("Could create request")    
+	log.Fatal("Could create request", err)    
     }
     query := request.URL.Query()
-    query.Add("client_id", os.Getenv("CLIENT_ID"))
-    query.Add("scope", "channels:read,channels:write")
+    query.Add("token", os.Getenv("TOKEN"))
     request.URL.RawQuery = query.Encode()
+    response, err := client.Do(request)
+    if (err != nil) {
+        log.Fatal("Failed to get websocket", err)
+    }
+    defer response.Body.Close()
+    var slackAuthResponse SlackAuthResponse
+    decoder := json.NewDecoder(response.Body)
+    err = decoder.Decode(&slackAuthResponse)
+    if (err != nil) {
+        log.Fatal("Failure decoding JSON")
+    }
+    websocketUrl := slackAuthResponse.Url
+    log.Printf("Websocket: %s", websocketUrl)
+
+    ws, err := websocket.Dial(websocketUrl, "", "http://localhost")
+    if (err != nil) {
+        log.Fatal("Failed opening websocket", err)
+    }
+    ws.Write([]byte("Hello, This is gobot!\n"));
+    
 }
