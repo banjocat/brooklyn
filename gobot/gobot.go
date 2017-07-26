926b1c0777110f322f843828a2ec7b1561e2b256
banjocat <jackmuratore@gmail.com>
2017-07-26 01:57:01 -0400

    Responds via matches in config.json

--- gobot/gobot.go
+++ gobot/gobot.go
@@ -7,6 +7,8 @@ import (
     "encoding/json"
     "strings"
     "github.com/gorilla/websocket"
+    "io/ioutil"
+    "regexp"
 )
 
 type SlackAuthResponse struct{
@@ -23,16 +25,14 @@ type SlackMessage struct{
     Text string	    `json:"text"`
 }
 
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+type Config struct{
+    Conversation map[string]string
 }
 
-
-const slackRtmAuthorize = "https://slack.com/api/rtm.connect"
-
-func readMessage() {
+func init() {
+    log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
+
 func main() {
     /*
     handshake to slack
@@ -40,6 +40,56 @@ func main() {
     parse messages directed at bot
     send repsonses based on regex
     */
+    websocketUrl, botId, err := getSocketUrlAndBotId()
+    if (err != nil) {
+	log.Fatal(err)
+    }
+    conn, _, err := websocket.DefaultDialer.Dial(websocketUrl, nil)
+    if (err != nil) {
+	log.Fatal(err)
+    }
+    config, err := getConfig()
+    if (err != nil) {
+	log.Fatal(err)
+    }
+    defer conn.Close()
+    for {
+	_, message, err := conn.ReadMessage()
+	if (err != nil) {
+	    log.Println(err)
+	    continue
+	}
+	ok, eventChannel, eventMessage := validEvent(message, botId)
+	if !ok {
+	    continue
+	}
+	log.Printf("recv: %s", message)
+	messageToSend := determineMessage(eventMessage, config)
+	if messageToSend == ""{
+	    continue
+	}
+	slackMessage := SlackMessage{
+	    Id: 1,
+	    Type: "message",
+	    Channel: eventChannel,
+	    Text: messageToSend,
+	}
+	slackMessageJson, err := json.Marshal(slackMessage)
+	log.Printf("sending: %s", slackMessageJson)
+	conn.WriteMessage(websocket.TextMessage, slackMessageJson)
+    }
+    defer conn.Close()
+}
+
+func getConfig() (Config, error) {
+    configJson, err := ioutil.ReadFile("./config.json")
+    var config Config
+    err = json.Unmarshal(configJson, &config)
+    return config, err
+}
+
+func getSocketUrlAndBotId() (string, string, error) {
+    const slackRtmAuthorize = "https://slack.com/api/rtm.connect"
     client := &http.Client{}
     request, err := http.NewRequest("GET", slackRtmAuthorize, nil)
     if (err != nil) {
@@ -53,57 +103,59 @@ func main() {
 	log.Fatal(err)
     }
     defer response.Body.Close()
-    
+
     var slackAuthResponse SlackAuthResponse
     decoder := json.NewDecoder(response.Body)
     err = decoder.Decode(&slackAuthResponse)
     if (err != nil) {
 	log.Fatal(err)
     }
-    // Looking for <@BotId> in message to see if mentioned 
-    botIdtag := "<@" + slackAuthResponse.Self["id"] + ">"
     websocketUrl := slackAuthResponse.Url
+    botId := slackAuthResponse.Self["id"]
     log.Printf("Websocket: %s", websocketUrl)
-    conn, _, err := websocket.DefaultDialer.Dial(websocketUrl, nil)
-    if (err != nil) {
-	log.Fatal(err)
-    }
-    defer conn.Close()
-    for {
-	_, message, err := conn.ReadMessage()
-	if (err != nil) {
-	    log.Println(err)
-	    continue
-	}
-	log.Printf("recv: %s", message)
-	var event map[string]interface{}
-	err = json.Unmarshal(message, &event)
-	if (err != nil) {
-	    log.Println(err)
-	    continue
-	}
-
-	eventTypeInterface, ok := event["type"]
-	if !ok {
-	    continue
-	}
-	eventType := eventTypeInterface.(string)
-	if eventType != "message" {
-	    continue
-	}
-	eventMessage := event["text"].(string)
-	if !strings.Contains(eventMessage, botIdtag) {
-	    continue
-	}
-	slackMessage := SlackMessage{
-	    Id: 1,
-	    Type: "message",
-	    Channel: event["channel"].(string),
-	    Text: "Yeah I got that",
-	}
-	slackMessageJson, err := json.Marshal(slackMessage)
-	log.Printf("sending: %s", slackMessageJson)
-	conn.WriteMessage(websocket.TextMessage, slackMessageJson)
-    }
-    defer conn.Close()
+    return websocketUrl, botId, nil
 }
+
+func validEvent(message []byte, botId string) (bool, string, string) {
+    var event map[string]interface{}
+    err := json.Unmarshal(message, &event)
+    if (err != nil) {
+	log.Println(err)
+	return false, "", ""
+    }
+    eventTypeInterface, ok := event["type"]
+    if !ok {
+	return false, "", ""
+    }
+    eventType := eventTypeInterface.(string)
+    if eventType != "message" {
+	return false, "", ""
+    }
+    eventMessage := event["text"].(string)
+    if !strings.Contains(eventMessage, botId) {
+	return false, "", ""
+    }
+    user := event["user"].(string)
+    if user == botId {
+	return false, "", ""
+    }
+    eventChannel := event["channel"].(string)
+    return true, eventChannel, eventMessage
+}
+
+func determineMessage(eventMessage string, config Config) (string) {
+    for k, v := range config.Conversation {
+	log.Printf("k: %s v: %s", k, v)
+	reg := "(?i)" + k // Make it case insenstive
+	matched, err := regexp.Match(reg, []byte(eventMessage))
+	if (err != nil) {
+	    log.Print(err)
+	    return ""
+	}
+	if matched {
+	    return v
+	}
+    }
+    return ""
+}
+
