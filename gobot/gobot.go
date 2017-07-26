661a10a00bb650cbb68897c2464ad6659571fd8a
banjocat <jackmuratore@gmail.com>
2017-07-25 23:19:46 -0400

    Echoing slackbot

--- gobot/gobot.go
+++ gobot/gobot.go
@@ -5,7 +5,7 @@ import (
     "os"
     "net/http"
     "encoding/json"
-    "golang.org/x/net/websocket"
+    "github.com/gorilla/websocket"
 )
 
 type SlackAuthResponse struct{
@@ -14,9 +14,23 @@ type SlackAuthResponse struct{
     Team map[string]*string
 }
 
+type SlackMessage struct{
+    Id int	    `json:"id"`
+    Type string	    `json:"type"`
+    Channel string  `json:"channel"`
+    Text string	    `json:"text"`
+}
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
 
 const slackRtmAuthorize = "https://slack.com/api/rtm.connect"
 
+func readMessage() {
+}
 func main() {
     /*
     handshake to slack
@@ -27,29 +41,55 @@ func main() {
     client := &http.Client{}
     request, err := http.NewRequest("GET", slackRtmAuthorize, nil)
     if (err != nil) {
-	log.Fatal("Could create request", err)    
+	log.Fatal(err)
     }
     query := request.URL.Query()
     query.Add("token", os.Getenv("TOKEN"))
     request.URL.RawQuery = query.Encode()
     response, err := client.Do(request)
     if (err != nil) {
-        log.Fatal("Failed to get websocket", err)
+	log.Fatal(err)
     }
     defer response.Body.Close()
     var slackAuthResponse SlackAuthResponse
     decoder := json.NewDecoder(response.Body)
     err = decoder.Decode(&slackAuthResponse)
     if (err != nil) {
-        log.Fatal("Failure decoding JSON")
+	log.Fatal(err)
     }
     websocketUrl := slackAuthResponse.Url
     log.Printf("Websocket: %s", websocketUrl)
-
-    ws, err := websocket.Dial(websocketUrl, "", "http://localhost")
+    conn, _, err := websocket.DefaultDialer.Dial(websocketUrl, nil)
     if (err != nil) {
-        log.Fatal("Failed opening websocket", err)
+	log.Fatal(err)
     }
-    ws.Write([]byte("Hello, This is gobot!\n"));
-    
+    defer conn.Close()
+    for {
+	_, message, err := conn.ReadMessage()
+	if (err != nil) {
+	    log.Println(err)
+	    continue
+	}
+	log.Printf("recv: %s", message)
+	var event map[string]interface{}
+	err = json.Unmarshal(message, &event)
+	if (err != nil) {
+	    log.Println(err)
+	    continue
+	}
+	eventType := event["type"].(string)
+	if eventType != "message" {
+	    continue
+	}
+	slackMessage := SlackMessage{
+	    Id: 1,
+	    Type: "message",
+	    Channel: event["channel"].(string),
+	    Text: event["text"].(string),
+	}
+	slackMessageJson, err := json.Marshal(slackMessage)
+	log.Printf("sending: %s", slackMessageJson)
+	conn.WriteMessage(websocket.TextMessage, slackMessageJson)
+    }
+    defer conn.Close()
 }
